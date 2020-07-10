2ec907aea5f9f279953621efd63319527c3650a3
unknown <brunomoreno7538@gmail.com>
2020-07-10 08:56:50 -0300

    chore: add jest configs and scripts

---
+++ jest-integration-config.js
@@ -0,0 +1,3 @@
+const config = require('./jest.config')
+config.testEnvironment = ['**/*.test.ts']
+module.exports = config
---
+++ jest-unit-config.js
@@ -0,0 +1,3 @@
+const config = require('./jest.config')
+config.testEnvironment = ['**/*.spec.ts']
+module.exports = config
